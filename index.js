d31b884ff34741dcf40ab19c9002046e0e079f8b
GLMahon99 <gastimahon@gmail.com>
2025-08-27 16:51:26 -0300

    nuevos cambios en conexion db

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ const port = 8000;
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME,
-    port: process.env.MYSQL_PORT,
-    ssl: {
-        rejectUnauthorized: false // Permite certificados autofirmados
-    }
+  connectionLimit: 10,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: { rejectUnauthorized: false }
 });
 
 
