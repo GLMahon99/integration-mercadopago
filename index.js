56cf5636fe4d1d87f114b24c64a801a6d6528906
GLMahon99 <gastimahon@gmail.com>
2025-08-27 17:00:46 -0300

    agrego logs para determinar error

--- index.js
+++ index.js
@@ -14,13 +14,21 @@ const port = 8000;
 
 dotenv.config();
 
+console.log("Variables de entorno:");
+console.log("MYSQLHOST:", process.env.MYSQLHOST);
+console.log("MYSQLPORT:", process.env.MYSQLPORT);
+console.log("MYSQLUSER:", process.env.MYSQLUSER);
+console.log("MYSQLPASSWORD:", process.env.MYSQLPASSWORD);
+console.log("MYSQLDATABASE:", process.env.MYSQLDATABASE);
+
+
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  port: Number(process.env.MYSQLPORT),
   ssl: { rejectUnauthorized: false }
 });
 
